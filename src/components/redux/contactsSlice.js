7bd9cb8af98c04fef12144f438a25f6759445e4a
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-20 17:43:04 +0200

    contactsSlice updated

--- src/components/redux/contactsSlice.js
+++ src/components/redux/contactsSlice.js
@@ -1,18 +1,48 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'https://64b8f98579b7c9def6c05a4e.mockapi.io';
 
-export const addContact = createAction('contacts/addContact');
-export const deleteContact = createAction('contacts/deleteContact');
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async () => {
+    const response = await axios.get('/contacts');
+    return response.data;
+  }
+);
 
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contactData) => {
+    const response = await axios.post('/contacts', contactData);
+    return response.data;
+  }
+);
 
-const contactsReducer = createReducer([], builder => {
-  builder
-    .addCase(addContact, (state, action) => {
-      state.push(action.payload);
-    })
-    .addCase(deleteContact, (state, action) => {
-      return state.filter(contact => contact.id !== action.payload);
-    });
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId) => {
+    await axios.delete(`/contacts/${contactId}`);
+    return contactId;
+  }
+);
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.push(action.payload);
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        return state.filter((contact) => contact.id !== action.payload);
+      });
+  },
 });
 
-export default contactsReducer;
+export default contactsSlice.reducer;
