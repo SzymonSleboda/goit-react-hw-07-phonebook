57151f32774a4d1740598e0661a7202e642df617
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-20 18:57:32 +0200

    refactored App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import s from './App.module.css';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from './redux/contactsSlice';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className={s.container}>
-      <section title="Phonebook" className={s.section}>
+      <section className={s.section}>
         <h1>Phonebook</h1>
         <ContactForm />
       </section>
 
-      <section title="Contacts" className={s.section}>
+      <section className={s.section}>
         <h2>Contacts</h2>
         <Filter />
         <ContactList />
